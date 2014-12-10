094344b670db1cf3c75484f0d3fca49c9dfaad31
hogesuke <miyado@gmail.com>
2014-12-10 23:06:05 +0900

    GET /entry  の仮実装

--- app.rb
+++ app.rb
@@ -1,8 +1,23 @@
+# coding: utf-8
+
 require 'sinatra'
 require 'sinatra/reloader'
+require 'net/http'
+require 'xmlsimple'
+require 'json'
 
 get '/entry' do
   # todo エントリーを取得する
+  url = URI.parse('http://b.hatena.ne.jp/search/tag?q=ruby&mode=rss')
+  req = Net::HTTP::Get.new(url.path + '?' + url.query)
+  res = Net::HTTP.start(url.host, url.port) {|http|
+    http.request(req)
+  }
+
+  entries = XmlSimple.xml_in(res.body)['item']
+
+  headers({'Content-Type' => 'application/json'})
+  entries.to_json
 end
 
 post '/entry/checked' do
