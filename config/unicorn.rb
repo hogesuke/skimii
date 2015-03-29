3a52ea0136850702abfa28965ac42b979374017a
hogesuke <miyado@gmail.com>
2015-03-29 16:52:27 +0900

    unicornの設定ファイルを追加

---
+++ config/unicorn.rb
@@ -0,0 +1,12 @@
+@dir = File.join(__dir__, './')
+
+worker_processes 2
+working_directory @dir
+
+timeout 300
+listen 4567
+
+pid "#{@dir}tmp/pids/unicorn.pid"
+
+stderr_path "#{@dir}log/unicorn.stderr.log"
+stdout_path "#{@dir}log/unicorn.stdout.log"
